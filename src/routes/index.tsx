0b35660938d12566fadef5408486f274c601aa38
illispi <44943503+illispi@users.noreply.github.com>
2024-09-20 15:22:40 +0300

    progress

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,24 @@
-import { A } from "@solidjs/router";
+import { A, cache, createAsync } from "@solidjs/router";
+import { For, Show, Suspense } from "solid-js";
 import { Button } from "~/components/ui/button";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import { readFile } from "fs/promises";
+
+const getShortcuts = cache(async () => {
+	"use server";
+	const data = JSON.parse(
+		await readFile("src/shortcuts/shortcuts.json", "utf8"),
+	);
+	return data;
+}, "shortcuts");
+
+export const route = {
+	load: () => getShortcuts(),
+};
 
 export default function Home() {
+	const shortcuts = createAsync(() => getShortcuts());
+	// console.log(shortcuts(), "hello");
 	return (
 		<div class="text-center flex-col flex max-w-screen-lg mx-auto text-gray-700 p-4">
 			<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
@@ -16,6 +33,13 @@ export default function Home() {
 					<img src="https://www.google.com/s2/favicons?domain=reddit.com" />
 					Reddit home
 				</Button>
+				<Suspense>
+					<Show when={shortcuts()}>
+						<For each={shortcuts().shortcuts}>
+							{(item) => <div>{item.name}</div>}
+						</For>
+					</Show>
+				</Suspense>
 			</div>
 		</div>
 	);
